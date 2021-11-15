41f8493c8d14bc8a2df53a85bbfe8439e733cfac
vaibhavi173 <72758759+vaibhavi173@users.noreply.github.com>
2021-11-15 15:27:28 +0530

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -2,13 +2,15 @@
 echo "Enter the year"
 read y
 if [ `expr $y % 100` -eq 0 ]; then
-	    if [ `expr $y % 4` -eq 0 ]; then
-           echo "Given year is not a leap year"
-            else
-		echo "      
+	if [ `expr $y % 4` -eq 0 ]; then
+           	echo "Leap year"
+        else
+		echo "Not a leap year" 
+	fi
 else
-    if [ `expr $y % 400` -eq 0 ]; then
-    else
-          echo "Given year is not a leap year"
-    fi
+ 	if [ `expr $y % 400` -eq 0 ]; then
+    	echo "Leap year"
+	else
+    	echo "Not a leap year"
+        fi
 fi
