3cca50d436768bb6e9e91d30fd493423269219e1
vaibhavi173 <72758759+vaibhavi173@users.noreply.github.com>
2021-11-15 11:39:24 +0530

    Create lab11.sh

---
+++ lab11.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Enter the number : "
+read n
+echo "Enter the power of number"
+read pow
+count=1
+ans=1
+while [ $count -le $n ]
+do
+ans=$((ans*n))
+count=$((count+1))
+done 
+echo "Power $pow of number $n is : "
+echo $ans
