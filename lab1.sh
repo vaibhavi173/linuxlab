7cd321540501d30ad1b4190a223e001321e0be67
vaibhavi173 <72758759+vaibhavi173@users.noreply.github.com>
2021-12-05 11:37:57 +0530

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -8,7 +8,7 @@ if [ `expr $y % 100` -eq 0 ]; then
 		echo "Not a leap year" 
 	fi
 else
- 	if [ `expr $y % 400` -eq 0 ]; then
+ 	if [ `expr $y % 4` -eq 0 ]; then
     	echo "Leap year"
 	else
     	echo "Not a leap year"
